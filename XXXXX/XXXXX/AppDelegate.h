3f778949b622105972d5e860a8742903d51cf884
go <chenwl@duzhesm.com>
2017-05-04 18:01:34 +0800

    github初体验

---
+++ XXXXX/XXXXX/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  XXXXX
+//
+//  Created by chen on 4/5/17.
+//  Copyright © 2017年 XU. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ XXXXX/XXXXX/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  XXXXX
+//
+//  Created by chen on 4/5/17.
+//  Copyright © 2017年 XU. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
